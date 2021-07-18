17ceb59c2473704326de043b335c1f5aff46e09a
nguyenthao1988 <hoangtunho2015@gmail.com>
2021-07-18 10:30:34 +0700

    upload nuget version 1.0

--- SqlHelperLib/Properties/AssemblyInfo.cs
+++ SqlHelperLib/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SqlHelperLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Thư viện kết nối Sqlserver + Dapper")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("LAPTRINHVB.NET")]
 [assembly: AssemblyProduct("SqlHelperLib")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
